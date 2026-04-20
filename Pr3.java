368e3ec807ed88be006febed2181177a8fda477c
krinalkakadiya2-lang <krinalkakadiya2@gmail.com>
2026-04-20 13:14:59 +0530

    Pr3

---
+++ Pr3.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class VowelConsonant {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a single letter: ");
+        char ch = sc.next().charAt(0);
+
+  
+        ch = Character.toLowerCase(ch);
+
+        
+        if (ch >= 'a' && ch <= 'z') {
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+                System.out.println("It is a Vowel.");
+            } else {
+                System.out.println("It is a Consonant.");
+            }
+        } else {
+            System.out.println("Invalid input! Please enter a letter.");
+        }
+
+        sc.close();
+    }
+}/*
+Enter a single letter: A
+It is a Vowel.
+*/
