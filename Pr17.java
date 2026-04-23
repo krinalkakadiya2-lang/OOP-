0ed747931fa0842f943c279855f62c8560bf04f2
krinalkakadiya2-lang <krinalkakadiya2@gmail.com>
2026-04-23 17:22:41 +0530

    Pr17

---
+++ Pr17.java
@@ -0,0 +1,50 @@
+interface Exam {
+    boolean isPassed(int mark);
+}
+
+interface Classify {
+    String getDivision(double average);
+}
+
+class Result implements Exam, Classify {
+
+    public boolean isPassed(int mark) {
+        return mark >= 40;
+    }
+
+    public String getDivision(double average) {
+        if (average >= 60) {
+            return "First Division";
+        } else if (average >= 50) {
+            return "Second Division";
+        } else if (average >= 40) {
+            return "Third Division";
+        } else {
+            return "Fail";
+        }
+    }
+}
+
+public class pr17 {
+    public static void main(String[] args) {
+        Result r = new Result();
+
+        int marks = 55;
+        double average = 58.5;
+
+        if (r.isPassed(marks)) {
+            System.out.println("Student Passed with marks: " + marks);
+        } else {
+            System.out.println("Student Failed with marks: " + marks);
+        }
+
+        String division = r.getDivision(average);
+        System.out.println("Division: " + division);
+    }
+}
+
+/*
+Output:
+Student Passed with marks: 55
+Division: Second Division
+*/
